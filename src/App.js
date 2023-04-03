0eb2f0f73d8e008856684866245db4a60cbcc583
hrishi12345 <hrajgure6@gmail.com>
2023-04-03 12:04:20 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Input from './components/Users/input';
 import './App.css';
-
+import ItemList from './components/ItemList'
+import { useState } from 'react';
 function App() {
+  const [enterArray,setArray]=useState([])
+  const renderUser=(uname,uage)=>{
+    setArray((prevState)=>{
+      return [...prevState,{name:uname , age:uage}]
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lets get started</h1>
+      <Input adduser={renderUser}/>
+      <ItemList user={enterArray} />
     </div>
   );
 }
